d1c78cc9bbae9328bc7d959cd320909095c4b490
hagino3000 <hagino.3000@gmail.com>
2011-08-26 09:02:25 +0900

    Add oF addons

---
+++ lib/mac/ofxOpenNI/src/ofxOpenNI.h
@@ -0,0 +1,19 @@
+#ifndef	_H_OFXOPENNI
+#define _H_OFXOPENNI
+
+#include "ofxOpenNIContext.h"
+#include "ofxImageGenerator.h"
+#include "ofxIRGenerator.h"
+#include "ofxDepthGenerator.h"
+#include "ofxUserGenerator.h"
+#include "ofxTrackedUser.h"
+#include "ofxImageGenerator.h"
+#include "ofxHandGenerator.h"
+#include "ofxTrackedHand.h"
+#include "ofxOpenNIRecorder.h"
+
+#ifdef TARGET_OSX // only working on Mac at the moment
+#include "ofxHardwareDriver.h"
+#endif
+
+#endif
