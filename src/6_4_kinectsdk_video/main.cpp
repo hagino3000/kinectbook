5be569472bb0737201ccdd1848072c1f0d685bf2
hagino3000 <hagino.3000@gmail.com>
2011-08-27 19:16:00 +0900

    Fixed windows project (6_4)

--- src/6_4_kinectsdk_video/main.cpp
+++ src/6_4_kinectsdk_video/main.cpp
@@ -10,81 +10,81 @@
 
 int main() {
 
-    IplImage * image = NULL;
-    HRESULT hr;
+	IplImage * image = NULL;
+	HRESULT hr;
 
-    try {
-        // デバイスの初期化
-        hr = NuiInitialize(NUI_INITIALIZE_FLAG_USES_COLOR);
-        if (FAILED(hr)) {
-            throw std::runtime_error("Failed open the Kinect");
-        }
+	try {
+		// デバイスの初期化
+		hr = NuiInitialize(NUI_INITIALIZE_FLAG_USES_COLOR);
+		if (FAILED(hr)) {
+			throw std::runtime_error("Failed at initializing Kinect");
+		}
 
-        // ビデオカメラストリームのオープン
-        HANDLE nextImageEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
-        HANDLE nextFrameHandle = NULL;
-        hr = NuiImageStreamOpen(NUI_IMAGE_TYPE_COLOR, 
-                                NUI_IMAGE_RESOLUTION_640x480, 
-                                NULL, 2, nextImageEvent, 
-                                &nextFrameHandle);
-        if (FAILED(hr)) {
-            throw std::runtime_error("Failed at opening stream");
-        }
+		// ビデオカメラストリームのオープン
+		HANDLE nextImageEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+		HANDLE nextFrameHandle = NULL;
+		hr = NuiImageStreamOpen(NUI_IMAGE_TYPE_COLOR, 
+			                    NUI_IMAGE_RESOLUTION_640x480, 
+								NULL, 2, nextImageEvent, &nextFrameHandle);
+		if (FAILED(hr)) {
+			throw std::runtime_error("Failed at opening image stream");
+		}
 
-        // OpenCVの画像データの作成
-        DWORD width = 0, height = 0;
-        NuiImageResolutionToSize(NUI_IMAGE_RESOLUTION_640x480, 
-                                 width, height);
+		// OpenCVの画像データの作成
+		DWORD width = 0, height = 0;
+		NuiImageResolutionToSize(NUI_IMAGE_RESOLUTION_640x480, width, height);
 
-        image = cvCreateImage(cvSize(width, height), IPL_DEPTH_8U, 4);
+		image = cvCreateImage(cvSize(width, height), IPL_DEPTH_8U, 4);
+		if (image == 0) {
+			throw std::runtime_error("Failed at creating image");
+		}
 
-        while(1) {
+		while(1) {
 
-            // ビデオカメラ画像が更新されるのを待つ
-            WaitForSingleObject(nextImageEvent, INFINITE);
+			// ビデオカメラ画像が更新されるのを待つ
+			WaitForSingleObject(nextImageEvent, INFINITE);
 
-            // フレームの取得
-            CONST NUI_IMAGE_FRAME *imageFrame = 0;
-            hr = NuiImageStreamGetNextFrame(nextFrameHandle, 0, 
-                                            &imageFrame);
-            if (FAILED(hr)) {
-                std::cout << "Failed getting frame" << std::endl;
-                break;
-            }
+			// フレームの取得
+			CONST NUI_IMAGE_FRAME *imageFrame = 0;
+			hr = NuiImageStreamGetNextFrame(nextFrameHandle, 0, &imageFrame);
+			if (FAILED(hr)) {
+				std::cout << "Failed at getting next frame data" << std::endl;
+				break;
+			}
 
-            NuiImageBuffer * pTexture = imageFrame->pFrameTexture;
-            KINECT_LOCKED_RECT LockedRect;
-            pTexture->LockRect(0, &LockedRect, NULL, 0);
+			NuiImageBuffer * pTexture = imageFrame->pFrameTexture;
+			KINECT_LOCKED_RECT LockedRect;
+			pTexture->LockRect( 0, &LockedRect, NULL, 0 );
 
-            if(LockedRect.Pitch != 0){
-                BYTE * pBuffer = (BYTE*)LockedRect.pBits;
-                // メモリコピー
-                memcpy(image->imageData, pBuffer, 
-                       image->widthStep * image->height);
-                // 表示
-                cvShowImage("Video View", image);
-            } else {
-               // NOP
-            }
+			if( LockedRect.Pitch != 0 ){
+				BYTE * pBuffer = (BYTE*) LockedRect.pBits;
+				// メモリコピー
+				memcpy( image ->imageData, pBuffer, image->widthStep * image->height);
+				// 表示
+				cvShowImage("Video View", image);
+			} else {
+			   // NOP
+			}
 
-            // フレームバッファの解放
-            NuiImageStreamReleaseFrame(nextFrameHandle, imageFrame);
+			// フレームバッファの解放
+			NuiImageStreamReleaseFrame(nextFrameHandle, imageFrame);
 
-            // ESCもしくはqが押されたら終了させる
-            char key = cvWaitKey(10);
-            if (key == 27 || key == 'q') {
-                break;
-            }        
-        }
-    } catch(std::exception &ex) {
-        std::cout << ex.what() << std::endl;
-    }
+			// ESCもしくはqが押されたら終了させる
+			char key = cvWaitKey(10);
+			if (key == 27 || key == 'q') {
+				break;
+			}		
+		}
 
-    // デバイスのクローズ
-    NuiShutdown();
+	} catch(std::exception &ex) {
+		std::cout << ex.what() << std::endl;
+	}
 
-    if (image != NULL) {
-        cvReleaseImage(&image);
-    }
-    return 0;
+	// デバイスのクローズ
+	NuiShutdown();
+
+	if (image != NULL) {
+		cvReleaseImage(&image);
+	}
+	return 0;
 }
